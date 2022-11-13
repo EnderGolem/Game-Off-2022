b6a2db8a8946095d0498d8223fe4b50d2b2389cf
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-11-14 01:51:07 +0300

    Add RandomMove. Add MelleAttack

---
+++ Assets/Game/Scripts/AI/AiActionMoveRandomlyFromPoint.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Tools;
+using UnityEngine;
+
+public class AiActionMoveRandomlyFromPoint : AIAction
+{
+    [SerializeField]
+    protected Vector2 startPos;
+    [SerializeField]
+    protected bool isLeft = false;
+    [SerializeField]
+    protected float length;
+    protected AbilityMove _abilityMove;
+    public override void Initialization()
+    {
+        startPos = transform.position;
+        _abilityMove = GetComponent<AbilityMove>();
+    }
+    public override void PerformAction()
+    {
+        Move();
+    }
+
+    protected virtual void Move()
+    {
+        var direction = startPos;
+        if (isLeft)
+            direction.x -= length / 2;
+        else
+            direction.x += length / 2;
+        direction.y = 0;
+        direction.x -= transform.position.x;
+        if (transform.position.x > startPos.x + length / 2)
+            isLeft = true;
+        if (transform.position.x < startPos.x - length / 2)
+            isLeft = false;
+        _abilityMove.ProcessInput(direction.normalized);
+    }
+}
