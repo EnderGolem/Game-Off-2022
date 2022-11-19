f2766b64d5994fbd8a40fa49d60bb8d5f2f8182b
Observer189 <aleksandr.kozlovskiy03@mail.ru>
2022-11-19 22:48:15 +0300

    Melee attack animation and force damage improvement

--- Assets/Game/Scripts/ForceDamage.cs
+++ Assets/Game/Scripts/ForceDamage.cs
@@ -12,19 +12,25 @@ public class ForceDamage : MonoBehaviour
     protected float damageKoef;
 
     protected Health _health;
+    protected Rigidbody2D _rigidbody2D;
     private void Awake()
     {
         _health = GetComponent<Health>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        var diff = Mathf.Abs(other.relativeVelocity.magnitude) * other.collider.attachedRigidbody.mass;
+        var h = other.collider.GetComponent<Health>();
+        if (_health != null)
+        {
+            var diff = _rigidbody2D.velocity.magnitude*other.relativeVelocity.magnitude * _rigidbody2D.mass;
 
-        if (diff < minDiffToDamage) return;
-        
-        Debug.Log("Force: "+damageKoef * diff + gameObject.name);
-        
-        _health?.DoDamage(damageKoef * diff);
+            if (diff < minDiffToDamage) return;
+
+            Debug.Log("Force: " + damageKoef * diff + gameObject.name);
+
+            h?.DoDamage(damageKoef * diff);
+        }
     }
 }
