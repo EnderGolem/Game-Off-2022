9db826fee403f3ac08023dd5da10a8d5de6f2445
Chumakov123 <71515689+Chumakov123@users.noreply.github.com>
2022-11-27 12:47:40 +0300

    Add Explosion Effect
    Добавлена вспышка от взрыва и тряска экрана

--- Assets/Game/Scripts/Effects/ShakingCamera.cs
+++ Assets/Game/Scripts/Effects/ShakingCamera.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class ShakingCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public CinemachineVirtualCamera vcam;
+    CinemachineBasicMultiChannelPerlin noise;
+    public static ShakingCamera instance;
+
+    float _amplitude;
+    float _duration;
+    float _fadeCoef;
+    bool _shaking = false;
+
     void Start()
     {
-        
+        instance=this;
+        noise = vcam.GetComponentInChildren<CinemachineBasicMultiChannelPerlin>();
+        noise.m_FrequencyGain=0;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Shake(float amplitude = 1, float duration = 1)
     {
-        
+        _amplitude=amplitude;
+        _duration=duration;
+        _fadeCoef=_amplitude/_duration;
+        _shaking=true;
+        noise.m_AmplitudeGain=amplitude;
+        noise.m_FrequencyGain=1;
+    }
+    private void Update() {
+        if (_shaking)
+        {
+            _duration-=Time.deltaTime;
+            noise.m_AmplitudeGain-=_fadeCoef*Time.deltaTime;
+            if (_duration<=0)
+            {
+                _duration=0;
+                _shaking=false;
+                noise.m_FrequencyGain=0;
+            }
+        }
     }
 }
