fc3993641b79cdc1dce256b4990b87b2d82b5b13
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-11-28 23:29:48 +0300

    Balance(Vampire) : Балансировка.  Надеюсь в этот раз Гит никто не будет читать.

--- Assets/Game/Scripts/AI/AIDecisionTimeBetweenAction.cs
+++ Assets/Game/Scripts/AI/AIDecisionTimeBetweenAction.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using MoreMountains.Tools;
@@ -5,10 +6,19 @@ using UnityEngine;
 
 public class AIDecisionTimeBetweenAction : AIDecision
 {
+    [SerializeField]
     protected float lastTimeActive = 0;
 
     [Tooltip("Через какое повторое время, Десишион выдаст правду")]
     public float timeBetween;
+
+    [Tooltip("Через какое время, в первый раз используется данная способность.")]
+    public float timeBetweenStart;
+
+    private void Start()
+    {
+        lastTimeActive = -timeBetweenStart;
+    }
 
     public override bool Decide()
     {
--- Assets/Game/Scripts/SwapForm.cs
+++ Assets/Game/Scripts/SwapForm.cs
@@ -14,27 +14,38 @@ public class SwapForm : MonoBehaviour
     public string nameForm1;
     [Tooltip("Имя формы 2 на обьекте")] 
     public string nameForm2;
-    private int numForm;
 
-    
-    private Vector3 scale1; 
-    private Vector3 position1;
     
     
     [Tooltip("Размер обьекта 2")] 
     public Vector3 scale2;
     [Tooltip("Расположение обьекта 2")] 
     public Vector3 position2;
+    [Tooltip("Размер колайдера 2 обьекта")] 
+    public Vector2 scalecollider2;
+    [Tooltip("Оффсет 2 обьекта")] 
+    public Vector2 offsetcollider2;
+
+    protected Vector2 scalecollider1;
+    protected Vector2 offsetcollider1;
+    
+    private Vector3 scale1; 
+    private Vector3 position1;
     private bool startForm = true;
-
+    
+    
     protected Character _character;
-
+    protected CapsuleCollider2D _collider;
+    
     //Здесь должен быть старт, потому что, как я понял, inizialization вызывается до создания модельки и локаньные размеры будут 0 0 0 
     public void Start()
     {
         scale1 = transform.Find(nameForm1).localScale;
         position1 = transform.Find(nameForm1).localPosition;
         _character = GetComponent<Character>();
+        _collider = GetComponent<CapsuleCollider2D>();
+        scalecollider1 = _collider.size;
+        offsetcollider1 = _collider.offset;
     }
     public void changeForm()
     {
@@ -48,6 +59,8 @@ public class SwapForm : MonoBehaviour
             Destroy(transform.Find(nameForm1).gameObject);
             startForm = !startForm;
             _character.Animator = transform.Find(nameForm2).GetComponent<Animator>();
+            _collider.size = scalecollider2;
+            _collider.offset = offsetcollider2;
         }
         else
         {
@@ -59,6 +72,8 @@ public class SwapForm : MonoBehaviour
             Destroy(transform.Find(nameForm2).gameObject);
             startForm = !startForm;
             _character.Animator  = transform.Find(nameForm1).GetComponent<Animator>();
+            _collider.size = scalecollider1;
+            _collider.offset = offsetcollider1;
         }
     }
 }
